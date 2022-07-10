d65bd527b721384209ba807d4951aa1deebb61b6
Lewis <louislaizer4@gmail.com>
2022-07-10 19:36:32 +0300

    :boom: Base board structure

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="chess">
+    <div class="board">
+      <div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+          <div class="a1"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "../styles/chess.css";
+</style>
