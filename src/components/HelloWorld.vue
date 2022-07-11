f6da64f6b01141eaa1602ecf56cc2a818a5ff4e5
Lewis <louislaizer4@gmail.com>
2022-07-11 20:20:48 +0300

    :hammer: Pieces labels for testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,44 +13,24 @@ const count = ref(0);
     <div class="board">
       <div>
         <div class="boards-wr">
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
+          <div class="a1 light-sqaure">C</div> 
+          <div class="a1 dark-square">K</div>
+          <div class="a1 light-sqaure">B</div>
+          <div class="a1 dark-square">QN</div>
+          <div class="a1 light-sqaure">KN</div>
+          <div class="a1 dark-square">B</div>
+          <div class="a1 light-sqaure">K</div>
+          <div class="a1 dark-square">C</div>
         </div>
         <div class="boards-wr">
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-        </div>
-        <div class="boards-wr">
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-        </div>
-        <div class="boards-wr">
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
-          <div class="a1 dark-square"></div>
-          <div class="a1 light-sqaure"></div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
         </div>
         <div class="boards-wr">
           <div class="a1 light-sqaure"></div>
@@ -91,6 +71,26 @@ const count = ref(0);
           <div class="a1 light-sqaure"></div>
           <div class="a1 dark-square"></div>
           <div class="a1 light-sqaure"></div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1 light-sqaure">P</div> 
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+          <div class="a1 light-sqaure">P</div>
+          <div class="a1 dark-square">P</div>
+        </div>
+        <div class="boards-wr">
+          <div class="a1 dark-square">C</div>
+          <div class="a1 light-sqaure">K</div>
+          <div class="a1 dark-square">B</div>
+          <div class="a1 light-sqaure">QN</div>
+          <div class="a1 dark-square">KN</div>
+          <div class="a1 light-sqaure">B</div>
+          <div class="a1 dark-square">K</div>
+          <div class="a1 light-sqaure">C</div>
         </div>
       </div>
     </div>
